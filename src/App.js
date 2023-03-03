54d02f406114da88540cfe3ffe7cdb10aaa67aa9
Nikita Galibin <galibin24@mail.ru>
2023-03-03 13:46:51 +0000

    second

--- src/App.js
+++ src/App.js
@@ -6,18 +6,25 @@ const shaRegex = "^([0-9A-Fa-f]{2}[:]){19}([0-9A-Fa-f]{2})$";
 function App() {
 
   const [sha, setSha] = useState('');
+  const [result, setResult] = useState('');
   
-  function onClick() {
-    console.log(sha)
-  }
+  async function onClick() {
+    if (!shaRegex.match(sha)) {
+      throw new Error('BOOM')
+    }
 
+    const res = await fetch('http://example.com/movies.json')
+    setResult(res);
+  }
+ 
   return (
     <div className="App">
       <div >
 		   <div>Please input your sha below.</div>
-		   <input onChange={(e) => setSha(e.target.value)}></input>
-		   <button onClick={onClick}>Submit</button>
+        <input onChange={(e) => setSha(e.target.value)}></input>
+        <button onClick={onClick}>Submit</button>
       </div>
+      <div>{result}</div>
     </div>
   );
 }
