cf8c270fa78708dc0c975b6b16e8d09e246c43bf
Nikita Galibin <galibin24@mail.ru>
2023-03-03 15:19:27 +0000

    Co-authored-by: Nicolas Charon <nicolas-charon@users.noreply.github.com> Co-authored-by: Arty <artysidorenko@users.noreply.github.com> Co-authored-by: Rufus Oyemade <Oluwarufus@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,38 +1,48 @@
-import './App.css';
-import { useState } from 'react';
-
-//const shaRegex = /^([0-9A-Fa-f]{2}[:]){19}([0-9A-Fa-f]{2})$/;
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [sha, setSha] = useState("");
+  const [result, setResult] = useState("");
 
-  const [sha, setSha] = useState('');
-  const [result, setResult] = useState('');
-  
   async function onClick() {
-    // note: this won't be required if we hash it for them
-    // if (!shaRegex.match(sha)) {
-    //   throw new Error('BOOM')
-    // }
-	// generating hash
-	const encoder = new TextEncoder();
-  	const data = await encoder.encode(sha);
-    const hash = await crypto.subtle.digest('SHA-1', data);
-	const hashString = Array.from(new Uint8Array(hash)).map( x => x.toString(16).padStart(2,'0') ).join('');
-	console.log(hashString)
-	// fetch('https://example.com')
-    const res = await fetch(`https://europe-west2-linen-mapper-379416.cloudfunctions.net/FetchRelatedHashes?hash=${hashString}`)
-    setResult(res);
-	console.log(res)
+    // generating hash
+    const encoder = new TextEncoder();
+    const data = await encoder.encode(sha);
+    const hash = await crypto.subtle.digest("SHA-1", data);
+    const hashString = Array.from(new Uint8Array(hash))
+      .map((x) => x.toString(16).padStart(2, "0"))
+      .join("")
+      .toUpperCase();
+
+    const res = await fetch(
+      `https://europe-west2-linen-mapper-379416.cloudfunctions.net/FetchRelatedHashes?hash=${hashString.slice(
+        0,
+        5
+      )}`,
+      {
+        method: "GET",
+      }
+    );
+
+    const parsedResponse = await res.json();
+
+    const foundHash = parsedResponse.find((el) => {
+      console.log(el);
+      return el.hash === hashString;
+    });
+
+    setResult(foundHash);
   }
- 
+
   return (
     <div className="App">
-      <div >
-		   <div>Please input your password below.</div>
+      <div>
+        <div>Please input your password below.</div>
         <input onChange={(e) => setSha(e.target.value)}></input>
         <button onClick={onClick}>Submit</button>
       </div>
-      <div>{result}</div>
+      {result && <div>We found your hashed password {result.count} times</div>}
     </div>
   );
 }
