e46e70d4f48fbb0ec5e420a08602260603d9c24d
Nikita Galibin <galibin24@mail.ru>
2023-03-03 13:43:02 +0000

    Co-authored-by: Nicolas Charon <nicolas-charon@users.noreply.github.com> Co-authored-by: Arty <artysidorenko@users.noreply.github.com> Co-authored-by: Rufus Oyemade <Oluwarufus@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+const shaRegex = "^([0-9A-Fa-f]{2}[:]){19}([0-9A-Fa-f]{2})$";
 
 function App() {
+
+  const [sha, setSha] = useState('');
+  
+  function onClick() {
+    console.log(sha)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div >
+		   <div>Please input your sha below.</div>
+		   <input onChange={(e) => setSha(e.target.value)}></input>
+		   <button onClick={onClick}>Submit</button>
+      </div>
     </div>
   );
 }
